84a0e226efeb91d79652c485460c4f62ec29b6e7
FernPK <pornnapat5563@gmail.com>
2023-02-08 19:48:43 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Room from './roomComponent.js'
+import Room2 from './roomComponent2.js'
+import Room3 from './roomComponent3.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Miniproject</h1>
+      <Room className="box"></Room>
+      <Room2 className="box"></Room2>
+      <Room3 className="box"></Room3>
     </div>
   );
 }
---
+++ src/roomComponent2.js
@@ -0,0 +1,113 @@
+import './toggleBtnCss.css'
+import './roomCss.css'
+import { useEffect, useState } from 'react'
+
+const Room2 = () => {
+    const [mode, setMode] = useState(2)
+    const [onOff, setOnOff] = useState(0)
+    const [level, setLevel] = useState(0)
+    const handleChangeMode = (e) => {
+        //console.log(e.target.value)
+        if(e.target.value==="manual"){
+            setMode(2)
+        }
+        else{
+            setMode(1)
+        }
+    }
+    const handleOnOff = () => {
+        if(onOff===0){
+            setOnOff(1)
+        }
+        else{
+            setOnOff(0)
+        }
+        console.log(onOff)
+    }
+    const handleLevel = (e) => {
+        if(e.target.value==="100"){
+            setLevel(70)
+        }
+        else if(e.target.value==="200"){
+            setLevel(180)
+        }
+        else if(e.target.value==="300"){
+            setLevel(255)
+        }
+        console.log(level)
+    }
+    useEffect(()=>{
+        //event.preventDefault()
+    const payload = {
+        "id": 2,
+        "mode": mode,
+        "status": onOff,
+        "light": level
+    }
+
+    let status = 0
+    
+    fetch('http://group4.exceed19.online/update/front', {
+    method: 'PUT', // or 'PUT'
+    mode: 'cors',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+    })
+    .then((response) => {
+        status = response.status
+        return response.json()
+    })
+    .then((data) => {
+        if(status===200){
+            console.log(data.detail)
+        }
+        else if(status===422){
+            console.log("Please complete all fields.")
+        }
+        else{
+            console.log(data.detail)
+        }
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+    }, [mode, onOff, level])
+    return(
+        <div id="wrapper">
+            <h2>KITCHEN</h2>
+            <div id="modeDiv">
+                <p>Mode</p>
+                <div id="radioMode">
+                <div><input type="radio" id="manual" name="mode" value="manual" onChange={handleChangeMode}/>
+                <label for="manual">manual</label></div>
+                <div><input type="radio" id="auto" name="mode" value="auto"onChange={handleChangeMode}/>
+                <label for="auto">auto</label></div>
+                </div>
+            </div>
+            <div id="onOffDiv">
+                <p>Off</p>
+                <label class="switch" onClick={handleOnOff}>
+                <input type="checkbox"/>
+                <span class="slider round"></span>
+                </label>
+                <p>On</p>
+            </div>
+            <div id="brightnessDiv">
+                <p>Brightness Level</p>
+                <div id="radioBright">
+                    <input type="radio" id="low" name="bright" value="100" onChange={handleLevel}/>
+                    <label for="low">low</label>
+                    <input type="radio" id="mid" name="bright" value="200" onChange={handleLevel}/>
+                    <label for="mid">mid</label>
+                    <input type="radio" id="high" name="bright" value="300" onChange={handleLevel}/>
+                    <label for="high">high</label>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Room2
